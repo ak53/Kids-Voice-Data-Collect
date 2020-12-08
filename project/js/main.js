688a495761ebd70bf2187a4cf56da2d96ef3e9a5
Amandeep Kaur <aman@Amandeeps-MacBook-Air.local>
2020-12-09 01:41:02 +0530

    progress saving feature test

--- project/js/main.js
+++ project/js/main.js
@@ -44,8 +44,47 @@ function handleSignIn(){
 	  // The signed-in user info.
 	  var user = result.user;
 	  console.log(user.email)
-	  window.localStorage.setItem("user",user.email)
+	  var files_count=0;
+	  	var storageRef = firebase.storage().ref(user.email);
+	  	storageRef.listAll().then(function(result){
+	  		result.items.forEach(function(fileRef){
+	  			files_count++;
+	  		});
+	  	console.log(files_count);
+	  	console.log("change!!!")
+	  	window.localStorage.setItem("nextState",files_count+1)
+	  	// changePage()
+	  	}), function(error){
+	  		console.log(error)
+	  	};
+
 	  changePage()
+
+
+	  // prev = window.localStorage.getItem("user")
+	  // var files_count=0
+	  // if (prev){
+	  // 	var storageRef = firebase.storage().ref(prev);
+	  // 	storageRef.listAll().then(function(result){
+	  // 		result.items.forEach(function(fileRef){
+	  // 			files_count++;
+	  // 		});
+	  // 	console.log(files_count);
+	  // 	console.log("change!!!")
+	  // 	window.localStorage.setItem("nextState",files_count)
+	  // 	// changePage()
+	  // 	});
+	  // }else{
+	  // 	window.localStorage.setItem("user",user.email)
+	  // 	window.localStorage.setItem("nextState",1)
+	  // 	// changePage()
+
+	  // }
+	  // // changePage()
+
+
+
+
 
 	  // ...
 	}).catch(function(error) {
@@ -71,104 +110,3 @@ function changePage(){
 	loc = loc.concat("projects")
 	location.href=loc	
 }
-
-// function handleSubmit(){
-// 	console.log("hi")
-//   const input = document.querySelector('input');
-//   var file = input.files[0]
-//   console.log(file)
-//   var storage = firebase.storage();
-
-// 	// Create a storage reference from our storage service
-// 	var storageRef = storage.ref();
-// // File or Blob named mountains.jpg
-
-// 	// Create the file metadata
-// 	var metadata = {
-// 	  contentType: 'audio/mp3'
-// 	};
-
-// 	// Upload file and metadata to the object 'images/mountains.jpg'
-// 	var uploadTask = storageRef.child('audio/' + file.name).put(file, metadata);
-
-// 	// Listen for state changes, errors, and completion of the upload.
-// 	uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-// 	  function(snapshot) {
-// 	    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-// 	    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-// 	    console.log('Upload is ' + progress + '% done');
-// 	    switch (snapshot.state) {
-// 	      case firebase.storage.TaskState.PAUSED: // or 'paused'
-// 	        console.log('Upload is paused');
-// 	        break;
-// 	      case firebase.storage.TaskState.RUNNING: // or 'running'
-// 	        console.log('Upload is running');
-// 	        break;
-// 	    }
-// 	  }, function(error) {
-
-// 	  // A full list of error codes is available at
-// 	  // https://firebase.google.com/docs/storage/web/handle-errors
-// 	  switch (error.code) {
-// 	    case 'storage/unauthorized':
-// 	      // User doesn't have permission to access the object
-// 	      break;
-
-// 	    case 'storage/canceled':
-// 	      // User canceled the upload
-// 	      break;
-
-// 	    // ...
-
-// 	    case 'storage/unknown':
-// 	      // Unknown error occurred, inspect error.serverResponse
-// 	      break;
-// 	  }
-// 	}, function() {
-// 	  // Upload completed successfully, now we can get the download URL
-// 	  uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-// 	    console.log('File available at', downloadURL);
-// 	  });
-// 	});
-
-
-
-
-//   // let storageRef = firebase.storage().ref();
-//   // let metadata = {
-//   //   contentType: 'audio/mp3',
-//   // };
-//   // // let filePath = `${this.file.externalDataDirectory}` + `${this.fileName}`;
-//   //   file.readAsDataURL(file.externalDataDirectory).then((file) => {
-//   //   let voiceRef = storageRef.child(`voices/${this.fileName}`).putString(file, firebase.storage.StringFormat.DATA_URL);
-//   //   voiceRef.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot) => {
-//   //     console.log("uploading");
-//   //   }, (e) => {
-//   //     reject(e);
-//   //     console.log(JSON.stringify(e, null, 2));
-//   //   }, () => {
-//   //     var downloadURL = voiceRef.snapshot.downloadURL;
-//   //     resolve(downloadURL);
-//   //   });
-//   // });
-
-
-
-
-// 	// var data = $("#word").val(); 
-// 	// $("#word").val("")
-// 	// console.log("hi")
-// 	// console.log(data)
-// 	// addToDatabase(player, url);
-// }
-
-// function addToDatabase(player, url){
-// 	var postData = {
-// 		a : player,
-// 		b : url
-
-// 	}
-// 	var database = firebase.database().ref("posts");
-// 	var newPostRef = database.push();
-// 	newPostRef.set(postData);
-// }
